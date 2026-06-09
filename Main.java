e5a8dcb765a1f7bab35c8fc6de44386c0d5fd3c8
Gangani-art <ganganibuddhika89@gmail>
2026-06-09 10:57:00 +0530

    Add display,add student,search and average calculation method

--- Main.java
+++ Main.java
@@ -51,5 +51,73 @@ public class Main {
         } while(choice != 5);
     }
 
- 
+     public static void addStudent() {
+
+        System.out.print("Enter Student ID: ");
+        String id = input.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Marks: ");
+        double marks = input.nextDouble();
+
+        students.add(new Student(id, name, marks));
+
+        System.out.println("Student Added Successfully");
+    }
+
+    public static void displayStudents() {
+
+        for(Student s : students) {
+
+            System.out.println("ID: " + s.getStudentId());
+            System.out.println("Name: " + s.getStudentName());
+            System.out.println("Marks: " + s.getMarks());
+            System.out.println("------------------");
+        }
+    }
+
+    public static void searchStudent() {
+
+        System.out.print("Enter Student ID: ");
+        String id = input.nextLine();
+
+        boolean found = false;
+
+        for(Student s : students) {
+
+            if(s.getStudentId().equals(id)) {
+
+                System.out.println("Student Found");
+                System.out.println("Name: " + s.getStudentName());
+                System.out.println("Marks: " + s.getMarks());
+
+                found = true;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Student Not Found");
+        }
+    }
+
+    public static void calculateAverage() {
+
+        double total = 0;
+
+        for(Student s : students) {
+            total += s.getMarks();
+        }
+
+        if(students.size() > 0) {
+
+            double average = total / students.size();
+
+            System.out.println("Average Marks = " + average);
+        }
+        else {
+            System.out.println("No Students Available");
+        }
+    }
 }
